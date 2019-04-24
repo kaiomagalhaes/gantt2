7edc0920a0af976f33d9158229902d9a44c6d52b
kaiomagalhaes <me@kaiomagalhaes.com>
2019-04-24 14:28:08 +0200

    run prettier on the src files

--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,23 @@ import uglify from 'rollup-plugin-uglify';
 import merge from 'deepmerge';
 
 const dev = {
-    input: 'src/index.js',
-    output: {
-        name: 'Gantt',
-        file: 'dist/frappe-gantt.js',
-        format: 'iife'
-    },
-    plugins: [
-        sass({
-            output: 'dist/frappe-gantt.css'
-        })
-    ]
+  input: 'src/index.js',
+  output: {
+    name: 'Gantt',
+    file: 'dist/frappe-gantt.js',
+    format: 'iife'
+  },
+  plugins: [
+    sass({
+      output: 'dist/frappe-gantt.css'
+    })
+  ]
 };
 const prod = merge(dev, {
-    output: {
-        file: 'dist/frappe-gantt.min.js'
-    },
-    plugins: [uglify()]
+  output: {
+    file: 'dist/frappe-gantt.min.js'
+  },
+  plugins: [uglify()]
 });
 
 export default [dev, prod];
--- src/utils/enums/view_modes.js
+++ src/utils/enums/view_modes.js
@@ -1,6 +1,6 @@
-export const DAY = 'Day';
-export const HALF_DAY = 'Half Day';
-export const MONTH = 'Month';
-export const QUARTER_DAY = 'Quarter Day';
-export const WEEK = 'Week';
-export const YEAR = 'Year';
+export const DAY = 'Day'
+export const HALF_DAY = 'Half Day'
+export const MONTH = 'Month'
+export const QUARTER_DAY = 'Quarter Day'
+export const WEEK = 'Week'
+export const YEAR = 'Year'
