b8d85bd21a6ce65403a598dd49f1c60c08830aa4
kaiomagalhaes <me@kaiomagalhaes.com>
2019-04-28 08:38:30 +0200

    create tasks utils

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -1064,6 +1064,81 @@ const getDateIntervalRange = (period, viewScale, withPadding, step) => {
   return dates;
 };
 
+const getPreparedTasks = tasks => {
+  const preparedTasks = tasks.map((task, i) => {
+    // convert to Date objects
+    task._start = date_utils.parse(task.start);
+    task._end = date_utils.parse(task.end);
+
+    // make task invalid if duration too large
+    if (date_utils.diff(task._end, task._start, YEAR) > 10) {
+      task.end = null;
+    }
+
+    // cache index
+    task._index = i;
+
+    // invalid dates
+    if (!task.start && !task.end) {
+      const today = date_utils.today();
+      task._start = today;
+      task._end = date_utils.add(today, 2, DAY);
+    }
+
+    if (!task.start && task.end) {
+      task._start = date_utils.add(task._end, -2, DAY);
+    }
+
+    if (task.start && !task.end) {
+      task._end = date_utils.add(task._start, 2, DAY);
+    }
+
+    // if hours is not set, assume the last day is full day
+    // e.g: 2018-09-09 becomes 2018-09-09 23:59:59
+    const task_end_values = date_utils.get_date_values(task._end);
+    if (task_end_values.slice(3).every(d => d === 0)) {
+      task._end = date_utils.add(task._end, 24, HOUR);
+    }
+
+    // invalid flag
+    if (!task.start || !task.end) {
+      task.invalid = true;
+    }
+
+    // dependencies
+    if (isString(task.dependencies) || !task.dependencies) {
+      let deps = [];
+      if (task.dependencies) {
+        deps = task.dependencies
+          .split(',')
+          .map(d => d.trim())
+          .filter(d => d);
+      }
+      task.dependencies = deps;
+    }
+
+    // uids
+    if (!task.id) {
+      task.id = generate_id(task);
+    }
+
+    return task;
+  });
+
+  return preparedTasks;
+};
+
+const getTasksDependencies = tasks => {
+  const dependencies = {};
+  for (let t of tasks) {
+    for (let d of t.dependencies) {
+      dependencies[d] = dependencies[d] || [];
+      dependencies[d].push(t.id);
+    }
+  }
+  return tasks;
+};
+
 class Gantt {
   constructor(wrapper, tasks, options) {
     this.setup_wrapper(wrapper);
@@ -1150,77 +1225,8 @@ class Gantt {
 
   setup_tasks(tasks) {
     // prepare tasks
-    this.tasks = tasks.map((task, i) => {
-      // convert to Date objects
-      task._start = date_utils.parse(task.start);
-      task._end = date_utils.parse(task.end);
-
-      // make task invalid if duration too large
-      if (date_utils.diff(task._end, task._start, YEAR) > 10) {
-        task.end = null;
-      }
-
-      // cache index
-      task._index = i;
-
-      // invalid dates
-      if (!task.start && !task.end) {
-        const today = date_utils.today();
-        task._start = today;
-        task._end = date_utils.add(today, 2, DAY);
-      }
-
-      if (!task.start && task.end) {
-        task._start = date_utils.add(task._end, -2, DAY);
-      }
-
-      if (task.start && !task.end) {
-        task._end = date_utils.add(task._start, 2, DAY);
-      }
-
-      // if hours is not set, assume the last day is full day
-      // e.g: 2018-09-09 becomes 2018-09-09 23:59:59
-      const task_end_values = date_utils.get_date_values(task._end);
-      if (task_end_values.slice(3).every(d => d === 0)) {
-        task._end = date_utils.add(task._end, 24, HOUR);
-      }
-
-      // invalid flag
-      if (!task.start || !task.end) {
-        task.invalid = true;
-      }
-
-      // dependencies
-      if (isString(task.dependencies) || !task.dependencies) {
-        let deps = [];
-        if (task.dependencies) {
-          deps = task.dependencies
-            .split(',')
-            .map(d => d.trim())
-            .filter(d => d);
-        }
-        task.dependencies = deps;
-      }
-
-      // uids
-      if (!task.id) {
-        task.id = generate_id(task);
-      }
-
-      return task;
-    });
-
-    this.setup_dependencies();
-  }
-
-  setup_dependencies() {
-    this.dependency_map = {};
-    for (let t of this.tasks) {
-      for (let d of t.dependencies) {
-        this.dependency_map[d] = this.dependency_map[d] || [];
-        this.dependency_map[d].push(t.id);
-      }
-    }
+    this.tasks = getPreparedTasks(tasks);
+    this.dependency_map = getTasksDependencies(tasks);
   }
 
   refresh(tasks) {
@@ -1907,16 +1913,6 @@ class Gantt {
   clear() {
     this.$svg.innerHTML = '';
   }
-}
-
-function generate_id(task) {
-  return (
-    task.name +
-    '_' +
-    Math.random()
-      .toString(36)
-      .slice(2, 12)
-  );
 }
 
 return Gantt;
---
+++ src/utils/tasks.js
@@ -0,0 +1,78 @@
+import { isString } from './string';
+import date_utils from '../date_utils';
+import { YEAR, DAY, HOUR } from './enums/time';
+
+export const getPreparedTasks = tasks => {
+  const preparedTasks = tasks.map((task, i) => {
+    // convert to Date objects
+    task._start = date_utils.parse(task.start);
+    task._end = date_utils.parse(task.end);
+
+    // make task invalid if duration too large
+    if (date_utils.diff(task._end, task._start, YEAR) > 10) {
+      task.end = null;
+    }
+
+    // cache index
+    task._index = i;
+
+    // invalid dates
+    if (!task.start && !task.end) {
+      const today = date_utils.today();
+      task._start = today;
+      task._end = date_utils.add(today, 2, DAY);
+    }
+
+    if (!task.start && task.end) {
+      task._start = date_utils.add(task._end, -2, DAY);
+    }
+
+    if (task.start && !task.end) {
+      task._end = date_utils.add(task._start, 2, DAY);
+    }
+
+    // if hours is not set, assume the last day is full day
+    // e.g: 2018-09-09 becomes 2018-09-09 23:59:59
+    const task_end_values = date_utils.get_date_values(task._end);
+    if (task_end_values.slice(3).every(d => d === 0)) {
+      task._end = date_utils.add(task._end, 24, HOUR);
+    }
+
+    // invalid flag
+    if (!task.start || !task.end) {
+      task.invalid = true;
+    }
+
+    // dependencies
+    if (isString(task.dependencies) || !task.dependencies) {
+      let deps = [];
+      if (task.dependencies) {
+        deps = task.dependencies
+          .split(',')
+          .map(d => d.trim())
+          .filter(d => d);
+      }
+      task.dependencies = deps;
+    }
+
+    // uids
+    if (!task.id) {
+      task.id = generate_id(task);
+    }
+
+    return task;
+  });
+
+  return preparedTasks;
+};
+
+export const getTasksDependencies = tasks => {
+  const dependencies = {};
+  for (let t of tasks) {
+    for (let d of t.dependencies) {
+      dependencies[d] = dependencies[d] || [];
+      dependencies[d].push(t.id);
+    }
+  }
+  return tasks;
+};
